2efc10a48be0afcbe0ad048f3189cd058de3b8e1
Noturno <Noturno@192.168.7.26>
2017-08-11 21:42:26 -0300

    commit

--- ExerciciosConectadoBanco5/src/AnimaisDao/CachorroDAO.java
+++ ExerciciosConectadoBanco5/src/AnimaisDao/CachorroDAO.java
@@ -1,10 +1,12 @@
 package AnimaisDao;
 
+import java.sql.Connection;
+
 public class CachorroDAO extends AnimalDAO {
 
-	public CachorroDAO(String nome, String cor, int qtdPatas) {
-		super(nome, cor, qtdPatas);
-		this.setRetornaSom("AuAu");
-	}
+	private final Connection con;
 
+	public CachorroDAO(Connection con) {
+		this.con = con;
+	}
 }
